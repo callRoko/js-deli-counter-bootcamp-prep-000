c43ef9c334e8b3b8142421277dff7dff24066462
Hazel Donaldson <hazel90.hd@gmail.com>
2019-04-12 16:20:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ function nowServing(katzDeliLine){
   for(i = 1; i < katzDeliLine.length; i++){
     if (katzDeliLine.length > 0){
       return(`Currently serving ${katzDeliLine[i]}.`)
-      
+       return katzDeliLine.shift()
     }
-    
+    else(katzDeliLine.length < 0){
+      return("There is nobody waiting to be served!")
+    }
   }
 }
 
